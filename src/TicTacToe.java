26c4aea0ca7e6d7b0e9319dac995051d70118e07
Francesco Ryu <francescoryu04@gmail.com>
2022-06-29 15:04:01 +0200

    Implemented Logic for player turn

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -3,9 +3,6 @@ import javax.swing.border.MatteBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.util.List;
 
 /**
  * @author: Francesco Ryu
@@ -19,6 +16,8 @@ public class TicTacToe extends JFrame {
     JPanel playGroundPanel;
     JButton[] btnArray = new JButton[9];
 
+    boolean player1Turn;
+
     public TicTacToe() {
 
         infoPanel = new JPanel();
@@ -28,7 +27,9 @@ public class TicTacToe extends JFrame {
         playGroundPanel = new JPanel();
         playGroundPanel.setLayout(new GridLayout(3, 3));
         for (int i = 0; i < 9; i++) {
-
+            btnArray[i] = new JButton();
+            btnArray[i].addActionListener(this::actionPerformed);
+            playGroundPanel.add(btnArray[i]);
         }
 
 
@@ -38,5 +39,24 @@ public class TicTacToe extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(333, 380);
         this.setVisible(true);
+
     }
+    public void actionPerformed(ActionEvent e) {
+        for (int i = 0; i < 9; i++) {
+            if (e.getSource() == btnArray[i]) {
+                if (player1Turn) {
+                    if (btnArray[i].getText().equals("")) {
+                        btnArray[i].setText("X");
+                        player1Turn = false;
+                    }
+                } else {
+                    if (btnArray[i].getText().equals("")) {
+                        btnArray[i].setText("O");
+                        player1Turn = true;
+                    }
+                }
+            }
+        }
+    }
+
 }
