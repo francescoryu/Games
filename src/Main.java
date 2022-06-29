8082dcbc36390641393163f4bcd89b7606a2def9
Francesco Ryu <francescoryu04@gmail.com>
2022-06-29 16:07:15 +0200

    added tictactoe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import javax.swing.*;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+
         new TicTacToe();
     }
 }
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -16,22 +16,31 @@ public class TicTacToe extends JFrame {
     JPanel playGroundPanel;
     JButton[] btnArray = new JButton[9];
 
+    JLabel label;
+
     boolean player1Turn;
 
     public TicTacToe() {
 
+        Font labelFont = new Font("Monaco", Font.BOLD, 20);
+
+
         infoPanel = new JPanel();
         infoPanel.setBorder(new MatteBorder(2, 2, 2, 2, Color.BLACK));
         infoPanel.setPreferredSize(new Dimension(0, 47));
+
+        label = new JLabel("Welcome");
+        label.setFont(labelFont);
+        infoPanel.add(label);
 
         playGroundPanel = new JPanel();
         playGroundPanel.setLayout(new GridLayout(3, 3));
         for (int i = 0; i < 9; i++) {
             btnArray[i] = new JButton();
             btnArray[i].addActionListener(this::actionPerformed);
+            btnArray[i].setBorder(new MatteBorder(1, 1, 1, 1, Color.BLACK));
             playGroundPanel.add(btnArray[i]);
         }
-
 
 
         this.add(infoPanel, BorderLayout.NORTH);
@@ -42,17 +51,26 @@ public class TicTacToe extends JFrame {
 
     }
     public void actionPerformed(ActionEvent e) {
+        Font buttonFont = new Font("Monaco", Font.BOLD, 50);
         for (int i = 0; i < 9; i++) {
             if (e.getSource() == btnArray[i]) {
                 if (player1Turn) {
                     if (btnArray[i].getText().equals("")) {
                         btnArray[i].setText("X");
+                        btnArray[i].setFont(buttonFont);
+                        label.setText("O Turn");
                         player1Turn = false;
+                        //btnArray[i].setEnabled(false);
+                        btnArray[i].setForeground(Color.BLACK);
                     }
                 } else {
                     if (btnArray[i].getText().equals("")) {
                         btnArray[i].setText("O");
+                        btnArray[i].setFont(buttonFont);
                         player1Turn = true;
+                        label.setText("X Turn");
+                        //btnArray[i].setEnabled(false);
+                        btnArray[i].setForeground(Color.RED);
                     }
                 }
             }
